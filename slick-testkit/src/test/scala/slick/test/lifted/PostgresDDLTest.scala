eb6df59e07bf7c7d28c160ca2e324aafde6c02c8
Stefan Holzmüller <stefan.holzmueller@ligadigital.com>
2018-06-14 13:19:16 +0200

    added failing tests for bug #1323

--- slick-testkit/src/test/scala/slick/test/lifted/PostgresDDLTest.scala
+++ slick-testkit/src/test/scala/slick/test/lifted/PostgresDDLTest.scala
@@ -5,18 +5,60 @@ import org.junit.Test
 
 /** Test case for the PostgresSQL DDL */
 class PostgresDDLTest {
-  import slick.jdbc.PostgresProfile.api._
 
-  @Test def testDoublePrecisionType: Unit = {
-    class T(tag: Tag) extends Table[Double](tag, "mytable") {
-      def num = column[Double]("num", O.PrimaryKey)
-
-      def * = num
-    }
-    val ts = TableQuery[T]
+  @Test def testPostgresTableWithPostgresStatement: Unit = {
+    import slick.jdbc.PostgresProfile.api._
+    val ts = TableQuery[TablesDefinedWithPostgresImport.TableWithDoubleColumn]
 
     val statement = ts.schema.createStatements.mkString
-    assertTrue("DDL (create) must contain DOUBLE PRECISION column: " + statement,
-      statement.contains("DOUBLE PRECISION"))
+    expectColumnType("DOUBLE PRECISION", statement)
+  }
+
+  @Test def testPostgresTableWithH2Statement: Unit = {
+    import slick.jdbc.H2Profile.api._
+    val ts = TableQuery[TablesDefinedWithPostgresImport.TableWithDoubleColumn]
+
+    val statement = ts.schema.createStatements.mkString
+    expectColumnType("DOUBLE", statement)
+  }
+
+  @Test def testH2TableWithPostgresStatement: Unit = {
+    import slick.jdbc.PostgresProfile.api._
+    val ts = TableQuery[TablesDefinedWithH2Import.TableWithDoubleColumn]
+
+    val statement = ts.schema.createStatements.mkString
+    expectColumnType("DOUBLE PRECISION", statement)
+  }
+
+  @Test def testH2TableWithH2Statement: Unit = {
+    import slick.jdbc.H2Profile.api._
+    val ts = TableQuery[TablesDefinedWithH2Import.TableWithDoubleColumn]
+
+    val statement = ts.schema.createStatements.mkString
+    expectColumnType("DOUBLE", statement)
+  }
+
+  def expectColumnType(columnType: String, statement: String) = {
+    assertTrue(s"DDL (create) must contain $columnType column: " + statement,
+      statement.contains(s"$columnType NOT NULL"))
+  }
+
+}
+
+object TablesDefinedWithPostgresImport {
+  import slick.jdbc.PostgresProfile.api._
+  class TableWithDoubleColumn(tag: Tag) extends Table[Double](tag, "postgrestable") {
+    def num = column[Double]("num", O.PrimaryKey)
+
+    def * = num
+  }
+}
+
+object TablesDefinedWithH2Import {
+  import slick.jdbc.H2Profile.api._
+  class TableWithDoubleColumn(tag: Tag) extends Table[Double](tag, "h2table") {
+    def num = column[Double]("num", O.PrimaryKey)
+
+    def * = num
   }
 }
